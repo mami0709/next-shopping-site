78bc9dc29305b87700480b70c37f01091f83f975
mami0709 <mmmxxxzelda@yahoo.co.jp>
2022-12-17 23:08:27 +0900

    initial commit

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  staticDirs: ['public'],
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react"
+}
